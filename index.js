d00b7a4518c2d7a13386d819b1484a4dc2cecfd0
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-07-21 14:27:24 +0900

    회원가입 기능 구현 & postman 테스트

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 const mongoose = require('mongoose');
+const { User } = require('./models/User');
+
+// application/json
+app.use(express.json());
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect(
@@ -20,6 +26,16 @@ mongoose
 
 app.get('/', (req, res) => {
   res.send('Hello world');
+});
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(PORT, () => {
--- models/user.js
+++ models/user.js
@@ -42,6 +42,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = {
-  User,
-};
+module.exports = { User };
